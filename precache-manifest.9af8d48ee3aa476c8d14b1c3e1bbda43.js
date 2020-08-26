22400438478d8e86216edc0dd329a0a4e8ca5d2c
Tina <tinaytini@yahoo.com>
2020-08-26 12:04:00 +0700

    Updates

---
+++ precache-manifest.9af8d48ee3aa476c8d14b1c3e1bbda43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06f255492322d53538ffafb3881b369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3711e24de25a57c288",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "adecd0b8d22dbc63308d",
+    "url": "/static/css/main.1d9b62f5.chunk.css"
+  },
+  {
+    "revision": "8c3711e24de25a57c288",
+    "url": "/static/js/2.1aff091b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.1aff091b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adecd0b8d22dbc63308d",
+    "url": "/static/js/main.7e9ef691.chunk.js"
+  },
+  {
+    "revision": "3e7c55d0643f4c0a7dc2",
+    "url": "/static/js/runtime-main.7ee24f4f.js"
+  }
+]);
